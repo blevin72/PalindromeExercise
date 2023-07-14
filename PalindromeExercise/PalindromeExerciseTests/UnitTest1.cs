abd9d606dfe19fdc25450b75273e602d0abe4b89
Bryan Levin <bryanlevin72@gmail.com>
2023-07-14 12:20:22 -0700

    Exercise 2

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("hannah", true)]
+        [InlineData("painting", false)]
+        [InlineData("racecar", true)]
+        public void Test1(string word, bool expected)
         {
+            //Arrange
+            WordSmith wordSmith = new WordSmith();
 
+            //Act
+            var actual = wordSmith.IsPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
